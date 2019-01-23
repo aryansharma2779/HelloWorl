cbe34a2ed881ace8c56667cb22070f7a7ff862f8
aryansharma2779 <aryan.sharma_da17@gla.ac.in>
2019-01-23 08:49:46 +0530

    queue

---
+++ src/main/queue.java
@@ -0,0 +1,54 @@
+package main;
+
+public class QueueADT
+{
+
+    int[] queue = new int[10];
+    int front = -1;
+    int rear = -1;
+    int size = queue.length;
+
+    void enqueue(int value)
+    {
+    }
+
+    int dequeue()
+    {
+        int deleted = 0;
+
+
+        return deleted;
+    }
+
+    int peek()
+    {
+        int item = 0;
+
+        return item;
+    }
+
+    int size()
+    {
+        int size = 0;
+        size = rear + 1;
+        return size;
+    }
+
+    boolean isEmpty()
+    {
+        if((front==-1) && (rear==-1))
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    boolean isFull()
+    {
+        return false;
+    }
+
+}
